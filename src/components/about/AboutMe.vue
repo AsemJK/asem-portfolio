c2fcb29ddd7838fce4100e719867ebc557e6311c
AsemJK <asemalsaiyadi@gmail.com>
2024-04-20 09:19:07 +0300

    new build

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -6,51 +6,51 @@ export default {
         {
           id: 1,
           bio: `
-				I am a .NET developer, specializing in using the .NET framework
-				to build applications. I am proficient in programming languages
-				such as C# and skilled in developing web, desktop,
-				and mobile applications using technologies like ASP.NET, MVC, .NET CORE, and Xamarin.
+                I am a .NET developer, specializing in using the .NET framework
+                to build applications. I am proficient in programming languages
+                such as C# and skilled in developing web, desktop,
+                and mobile applications using technologies like ASP.NET, MVC, .NET CORE, and Xamarin.
 
-				I am also a JavaScript developer, specializing in using JavaScript
-				to build web applications. I am proficient in front-end technologies
-				like HTML, CSS, and JavaScript frameworks
-				such as React, Angular, or Vue.js.
-				I am also skilled in back-end technologies like Node.js and Express.js.
+                I am also a JavaScript developer, specializing in using JavaScript
+                to build web applications. I am proficient in front-end technologies
+                like HTML, CSS, and JavaScript frameworks
+                such as React, Angular, or Vue.js.
+                I am also skilled in back-end technologies like Node.js and Express.js.
 
-			As a full-stack developer, I am proficient in both
-				front-end and back-end development.
-				I have a comprehensive understanding of both client-side and server-side
-				technologies and can work on all aspects of an application,
-				from the user interface to the database.
+              As a full-stack developer, I am proficient in both
+                front-end and back-end development.
+                I have a comprehensive understanding of both client-side and server-side
+                technologies and can work on all aspects of an application,
+                from the user interface to the database.
 
-				Both .NET and JavaScript development play crucial roles
-				in my software development journey, and being a full-stack developer
-				allows me to have a broader skill set and the ability
-				to work on various parts of an application.
+                Both .NET and JavaScript development play crucial roles
+                in my software development journey, and being a full-stack developer
+                allows me to have a broader skill set and the ability
+                to work on various parts of an application.
 						`,
         },
         {
           id: 2,
           bio: `I am a seasoned DevOps engineer with extensive experience
-			in managing software workflows using GitHub, AWS, and Azure. I have a deep understanding of Continuous Integration/Continuous Deployment (CI/CD) pipelines and have successfully implemented them using these platforms.
+                in managing software workflows using GitHub, AWS, and Azure. I have a deep understanding of Continuous Integration/Continuous Deployment (CI/CD) pipelines and have successfully implemented them using these platforms.
 
-			With GitHub, I have managed code repositories, reviewed code, and merged
-			pull requests to maintain code quality and consistency across the team.
-			I have also used GitHub Actions to automate software workflows,
-			enabling faster and more reliable deployments.
+                With GitHub, I have managed code repositories, reviewed code, and merged
+                pull requests to maintain code quality and consistency across the team.
+                I have also used GitHub Actions to automate software workflows,
+                enabling faster and more reliable deployments.
 
-			In AWS, I have leveraged services like AWS CodePipeline and AWS CodeDeploy
-			for building, testing, and deploying applications.
-			I have also used AWS CloudFormation for infrastructure as code,
-			allowing me to manage and provision AWS resources efficiently.
+                In AWS, I have leveraged services like AWS CodePipeline and AWS CodeDeploy
+                for building, testing, and deploying applications.
+                I have also used AWS CloudFormation for infrastructure as code,
+                allowing me to manage and provision AWS resources efficiently.
 
-			In Azure, I have used Azure DevOps for planning, developing, testing,
-			and delivering software. I have also used Azure Pipelines for CI/CD,
-			and Azure Resource Manager for managing and organizing resources.
+                In Azure, I have used Azure DevOps for planning, developing, testing,
+                and delivering software. I have also used Azure Pipelines for CI/CD,
+                and Azure Resource Manager for managing and organizing resources.
 
-			My experience in these areas has allowed me to streamline software development
-			processes, improve team collaboration, and increase deployment frequency,
-			all while maintaining high standards of quality and reliability.`,
+                My experience in these areas has allowed me to streamline software development
+                processes, improve team collaboration, and increase deployment frequency,
+                all while maintaining high standards of quality and reliability.`,
         },
       ],
     };
--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,32 +1,32 @@
 export const socialLinks = [
-	{
-		id: 1,
-		name: 'Website',
-		icon: 'globe',
-		url: 'https://asem-portfolio.onrender.com',
-	},
-	{
-		id: 2,
-		name: 'GitHub',
-		icon: 'github',
-		url: 'https://github.com/AsemJK',
-	},
-	{
-		id: 3,
-		name: 'Twitter',
-		icon: 'twitter',
-		url: 'https://twitter.com/#',
-	},
-	{
-		id: 4,
-		name: 'LinkedIn',
-		icon: 'linkedin',
-		url: 'https://www.linkedin.com/in/asemjk',
-	},
-	{
-		id: 5,
-		name: 'YouTube',
-		icon: 'youtube',
-		url: 'https://www.youtube.com/@asemjka',
-	},
+  {
+    id: 1,
+    name: 'Website',
+    icon: 'globe',
+    url: 'https://asemalsaiyadi.onrender.com',
+  },
+  {
+    id: 2,
+    name: 'GitHub',
+    icon: 'github',
+    url: 'https://github.com/AsemJK',
+  },
+  {
+    id: 3,
+    name: 'Twitter',
+    icon: 'twitter',
+    url: 'https://twitter.com/#',
+  },
+  {
+    id: 4,
+    name: 'LinkedIn',
+    icon: 'linkedin',
+    url: 'https://www.linkedin.com/in/asemjk',
+  },
+  {
+    id: 5,
+    name: 'YouTube',
+    icon: 'youtube',
+    url: 'https://www.youtube.com/@asemjka',
+  },
 ];
